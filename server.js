5bcc613b8513508f66a2799f179690661b00a714
Gabriel Gutierrez <gg447062@gmail.com>
2022-06-29 17:56:17 -0500

    adds /scripts route to deliver axios

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const dotenv = require('dotenv');
@@ -10,8 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/submit', submit);
+app.use(
+  '/scripts',
+  express.static(path.join(__dirname, 'node_modules/axios/dist'))
+);
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 const PORT = process.env.PORT || 3000;
 
