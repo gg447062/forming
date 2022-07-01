f8983b4d2e4e7aff0f1c7b540a1a2e2e877cc1ab
Gabriel Gutierrez <gg447062@gmail.com>
2022-06-30 23:26:39 -0500

    updates styles and adds basic form validation

--- public/main.js
+++ public/main.js
@@ -1,26 +1,56 @@
 const form = document.getElementById('form');
 const forming = document.getElementById('forming');
+const name = document.getElementById('name');
+const aboutMe = document.getElementById('aboutMe');
+const twitter = document.getElementById('twitter');
+const discord = document.getElementById('discord');
+const address = document.getElementById('address');
+const url = document.getElementById('url');
+const charCount = document.getElementById('charCount');
+const error = document.getElementById('error');
+const thankYou = document.getElementById('thank-you');
+const closeButton = document.getElementById('close');
+
+const isValidURL = (string) => {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+};
+
+const validateData = () => {
+  const inputs = Array.from(document.getElementsByClassName('form-input'));
+
+  if (inputs.filter((input) => !input.value).length) return false;
+
+  if (!isValidURL(url.value)) {
+    error.innerHTML = 'please add a valid url';
+    url.addEventListener('input', () => {
+      error.innerHTML = '';
+    });
+    return false;
+  }
+
+  return true;
+};
 
 const submitData = async () => {
   try {
-    const name = document.getElementById('name').value;
-    const aboutMe = document.getElementById('aboutMe').value;
-    const twitter = document.getElementById('twitter').value;
-    const discord = document.getElementById('discord').value;
-    const address = document.getElementById('address').value;
-    const url = document.getElementById('url').value;
-
-    const res = await axios.post('/submit', {
-      name,
-      aboutMe,
-      twitter,
-      discord,
-      address,
-      url,
+    await axios.post('/submit', {
+      name: name.value,
+      aboutMe: aboutMe.value,
+      twitter: twitter.value,
+      discord: discord.value,
+      address: address.value,
+      url: url.value,
     });
 
     form.reset();
-    alert(res.data);
   } catch (error) {
     console.log(error);
   }
@@ -28,14 +58,24 @@ const submitData = async () => {
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  await submitData();
+  const validated = validateData();
+
+  if (validated) {
+    await submitData();
+    thankYou.style.display = 'flex';
+  }
 });
 
-const moveForming = (y) => {
-  forming.style.letterSpacing = `${y}em`;
-};
-
-document.addEventListener('scroll', (e) => {
+document.addEventListener('scroll', () => {
   const y = (window.scrollY / 666 - 0.15).toFixed(2);
-  moveForming(y);
+  forming.style.letterSpacing = `${y}em`;
+});
+
+aboutMe.addEventListener('input', () => {
+  const count = aboutMe.value.length;
+  charCount.innerHTML = `${count}/280`;
+});
+
+closeButton.addEventListener('click', () => {
+  thankYou.style.display = 'none';
 });
