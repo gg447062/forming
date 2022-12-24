ab03e6028113330bb8aef28bc4cc781ca72a9f55
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2022-12-24 12:18:39 -0700

    adds date to submission data

--- server/api/submit.js
+++ server/api/submit.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   try {
     const { name, aboutMe, twitter, discord, address, url, email } = req.body;
+    const date = new Date().toLocaleDateString();
     const volume = 6;
 
     const reqData = {
@@ -19,6 +20,7 @@ router.post('/', async (req, res) => {
             'Video URL': url,
             Email: email,
             Volume: volume,
+            Date: date
           },
         },
       ],
