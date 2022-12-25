b72d60f152bdb7e031bfa19810013e1c623667e6
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2022-12-24 23:07:14 -0700

    changes redirect response code to 308

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '..', '/public')));
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-      res.redirect(301, `https://${req.headers.host}${req.originalUrl}`);
+      res.redirect(308, `https://${req.headers.host}${req.originalUrl}`);
     } else {
       next();
     }
