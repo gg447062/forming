0fea6ae2cfc533e59c59d14e065048aa32128b31
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-01-23 13:01:02 -0600

    adds app data to localstorage

--- app/data-context.js
+++ app/data-context.js
@@ -2,9 +2,28 @@ import React, { createContext, useState } from 'react';
 
 export const DataContext = createContext();
 
+const DATA_CACHE = 'DATA_CACHE';
+
+function checkStorage() {
+  const data = window.localStorage.getItem(DATA_CACHE);
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return false;
+  }
+}
+
+export function setStorage(data) {
+  localStorage.setItem(DATA_CACHE, JSON.stringify(data));
+}
+
 export function DataContextProvider({ children }) {
-  const [rsvp, setRsvp] = useState(false);
-  const [volume, setVolume] = useState(null);
+  const data = checkStorage();
+  const _rsvp = data ? data.rsvp : false;
+  const _volume = data ? data.volume : null;
+
+  const [rsvp, setRsvp] = useState(_rsvp);
+  const [volume, setVolume] = useState(_volume);
 
   const state = { rsvp, setRsvp, volume, setVolume };
 
